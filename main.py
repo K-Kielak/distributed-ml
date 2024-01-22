94a0262acf0a4721546412fabdcf89127a0f0375
Kacper Kielak <kielak.kacper@gmail.com>
2024-01-21 18:45:12 -0800

    Write overfitting model in PyTorch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
 from collections import deque
-from typing import Tuple
+from typing import Tuple, Optional
 
 import torch
 import torch.nn as tnn
@@ -14,13 +14,13 @@ DATA_PATH = "housing.csv"
 
 
 def main() -> None:
-    inputs, outputs = load_preprocessed_data(DATA_PATH)
+    inputs, outputs, out_mean, out_std = load_preprocessed_data(DATA_PATH)
     device = get_device()
     print(f"Inputs shape: {inputs.shape}")
     print(f"Outputs shape: {outputs.shape}")
     model = HousingNn().to(device)
     print(model)
-    train(model, inputs, outputs, device)
+    train(model, inputs, outputs, device, out_mean=out_mean, out_std=out_std)
 
 
 def train(
@@ -28,45 +28,56 @@ def train(
     inputs: NDArray[np.float64],
     outputs: NDArray[np.float64],
     device: str,
-    learning_rate: float = 1e-3,
-    batch_size: int = 256,
+    learning_rate: float = 1e-4,
+    batch_size: int = 512,
     max_epochs: int = 100,
     early_stopping_history_length: int = 10,
+    out_mean: Optional[float] = None,
+    out_std: Optional[float] = None,
 ) -> None:
     inputs = torch.tensor(inputs, dtype=torch.float32, device=device)
     outputs = torch.tensor(outputs, dtype=torch.float32, device=device)
     random_ordering = torch.randperm(len(inputs))
     inputs = inputs[random_ordering]
     outputs = outputs[random_ordering]
-    model.train()
     optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+    model.train()
     early_stopping_history = deque()  # for early stopping
 
     for epoch in range(max_epochs):
+        report_outputs = outputs
+        report_preds = model(inputs).squeeze()
+        report_mse = ((report_preds - outputs) ** 2).mean()
+        mae_suffix = ""
+        if out_mean is not None and out_std is not None:
+            report_preds = report_preds * out_std + out_mean
+            report_outputs = report_outputs * out_std + out_mean
+            mae_suffix = " (post destandarization)"
+
+        report_mae = (torch.abs(report_preds - report_outputs)).mean()
+        print(
+            f"Epoch {epoch + 1} will start. Current MAE{mae_suffix}: {report_mae.item():.3f}, MSE: {report_mse.item():.3f}"
+        )
+
         for batch in range(0, len(inputs), batch_size):
             batch_inputs = inputs[batch : batch + batch_size]
             batch_outputs = outputs[batch : batch + batch_size]
-            batch_predictions = model(batch_inputs)
+            batch_predictions = model(batch_inputs).squeeze()
+            assert batch_predictions.shape == batch_outputs.shape
+
             loss = ((batch_predictions - batch_outputs) ** 2).mean()
             loss.backward()
             optimizer.step()
             optimizer.zero_grad()
 
-        preds = model(inputs)
-        mae = (torch.abs(preds - outputs)).mean()
-        mse = ((preds - outputs) ** 2).mean()
-        print(
-            f"Epoch {epoch + 1} finished. MAE: {mae.item():.3f}, MSE: {mse.item():.3f}"
-        )
-
         if (
             len(early_stopping_history) == early_stopping_history_length
-            and np.mean(early_stopping_history) < mae
+            and np.mean(early_stopping_history) < report_mae
         ):
             print(f"Early stopping at epoch {epoch + 1}")
             break
 
-        early_stopping_history.append(mae.item())
+        early_stopping_history.append(report_mae.item())
         if len(early_stopping_history) > early_stopping_history_length:
             early_stopping_history.popleft()
 
@@ -77,9 +88,11 @@ class HousingNn(torch.nn.Module):
         self.layers = tnn.Sequential(
             tnn.Linear(13, 128),
             tnn.ReLU(),
-            tnn.Linear(128, 64),
+            tnn.Linear(128, 512),
             tnn.ReLU(),
-            tnn.Linear(64, 1),
+            tnn.Linear(512, 128),
+            tnn.ReLU(),
+            tnn.Linear(128, 1),
         )
 
     def forward(self, x):
@@ -100,20 +113,37 @@ def get_device() -> str:
 
 def load_preprocessed_data(
     data_path: os.PathLike,
-) -> Tuple[NDArray[np.float64], NDArray[np.float64]]:
+) -> Tuple[NDArray[np.float64], NDArray[np.float64], float, float]:
     data = pd.read_csv(data_path)
+
+    # Fill NaNs
     data["total_bedrooms"] = data["total_bedrooms"].fillna(
         data["total_bedrooms"].median()
     )
+
+    # One hot encode
     one_hot_proximity = pd.get_dummies(
         data["ocean_proximity"], prefix="ocean_proximity"
     ).astype(float)
     data = data.drop("ocean_proximity", axis=1)
+
+    # Standardize
+    mean = data.mean()
+    std = data.std()
+    data = (data - mean) / std
+
     data = data.join(one_hot_proximity)
     print(data.describe())
+
+    # Split
     outputs = data["median_house_value"]
     inputs = data.drop("median_house_value", axis=1)
-    return inputs.to_numpy(), outputs.to_numpy()
+    return (
+        inputs.to_numpy(),
+        outputs.to_numpy(),
+        mean["median_house_value"],
+        std["median_house_value"],
+    )
 
 
 if __name__ == "__main__":
